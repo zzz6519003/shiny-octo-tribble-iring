3b67f08e57326620835cecf2da92412efcd48bff
Zhengzhong Zhao <zhengzhongzhao@gmail.com>
2014-07-02 10:27:03 +0800

    Initial Commit

---
+++ iRing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iRing
+//
+//  Created by Snowmanzzz on 7/2/14.
+//  Copyright (c) 2014 Snowmanzzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
